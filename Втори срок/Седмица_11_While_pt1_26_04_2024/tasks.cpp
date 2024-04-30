04a550402fe2adac7368808751341544fd22fac2
Dimitar Shtregarski <58916033+DimitarSht@users.noreply.github.com>
2024-04-30 17:50:10 +0300

    Задачи за while-цикъл

---
+++ Втори срок/Седмица_11_While_pt1_26_04_2024/tasks.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, prod = 1;
+    /// Сума на числа
+//    cin>>n;
+//    while(n!=0)
+//    {
+//        sum+=n;
+//        cin>>n;
+//    }
+//    cout<<sum<<endl;
+//
+    /// Брой числа
+//    do
+//    {
+//        cin>>n;
+//        cnt++;
+//    }
+//    while(n!=0);
+//
+//    cout<<cnt<<endl;
+
+    /// Брой четни числа
+//    int cnt = 0;
+//    cin>>n;
+//    while(n!=0)
+//    {
+//        if(n%2==0)
+//        {
+//            cnt++;
+//        }
+////        prod*=n;    /// prod = prod * n;
+//        cin>>n;
+//    }
+//
+//    cout<<cnt<<endl;
+
+    /// Минимум и Максимум
+//    int Max = -1, Min = 100000;
+//    cin>>n;
+//    while(n!=0)
+//    {
+//        if(n > Max)
+//        {
+//            Max = n;
+//        }
+//        if(n < Min)
+//        {
+//            Min = n;
+//        }
+//        cin>>n;
+//    }
+//    cout<<Max<<" "<<Min<<endl;
+
+    /// Най-много еднакви последователни числа в редица
+    int MAX_cnt = 0, previousNumber;
+    cin>>n;
+    previousNumber = n;
+    while(n!=0)
+    {
+        cin>>n;
+        if(previousNumber == n)
+        {
+            MAX_cnt++;
+        }
+        previousNumber = n;
+    }
+
+    cout<<MAX_cnt<<endl;
+
+    return 0;
+}
