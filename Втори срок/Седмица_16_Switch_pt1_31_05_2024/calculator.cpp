ed2b691e1a77aaa762c514e7c85f62a64a5e0fbd
Dimitar Shtregarski <58916033+DimitarSht@users.noreply.github.com>
2024-06-05 08:39:38 +0300

    switch

---
+++ Втори срок/Седмица_16_Switch_pt1_31_05_2024/calculator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    char op;
+    cin>>op;
+    switch(op)
+    {
+    case '+':
+        cout<<a+b<<endl;
+        break;
+    case '-':
+        cout<<a-b<<endl;
+        break;
+    case '*':
+        cout<<a*b<<endl;
+        break;
+    case '/':
+        cout<<a/b<<endl;
+        break;
+    default:
+        cout<<"Vavedi drug operator!"<<endl;
+        break;
+    }
+    return 0;
+}
